16fafd3ffdeef8d43cc9741b9abd7a8f36862a2a
Ste <stephenkelehan@gmail.com>
2020-03-19 23:09:49 +0000

    Adding % circles and fixing images

--- src/templates/product.js
+++ src/templates/product.js
@@ -3,6 +3,7 @@ import "../css/main.css"
 import Nav from "../components/nav"
 import Footer from "../components/footer"
 import Markdown from "../components/markdown"
+import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 
 function showSectionA(show, title, body, id, className) {
@@ -19,13 +20,29 @@ function showSectionA(show, title, body, id, className) {
   }
 }
 
-const Product = ({ data }) => {
-  const {
-    title,
-    description,
-    featuredimage,
-    alias
-  } = data.markdownRemark.frontmatter
+const Product = () => {
+  const data = useStaticQuery(graphql`
+    query Photos {
+      images: allFile(
+        filter: { relativeDirectory: { eq: "intergated-products" } }
+      ) {
+        edges {
+          node {
+            id
+            childImageSharp {
+              fluid(maxWidth: 10000, quality: 100) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+            name
+          }
+        }
+      }
+    }
+  `)
+  console.log("hi")
+  console.log(data)
+
   return (
     <div>
       <Nav current={"CAPS"}></Nav>
@@ -51,23 +68,23 @@ const Product = ({ data }) => {
           </a>
         </div>
       </header>
-      <div className="product-main">
-        <div className="product-main-about">
-          <div className="product-main-about-title">About</div>
-          <div className="product-main-about-item">
-            <div className="product-main-about-item">
+      <div className="product-main container">
+        <div className="product-main-section-a">
+          <div className="product-main-section-a-title">About</div>
+          <div className="product-main-section-a-item">
+            <div className="product-main-section-a-item">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit.
               Accusantium, eligendi quia, mollitia nobis eaque voluptates fugiat
               molestiae perspiciatis cupiditate expedita temporibus neque modi
               velit nesciunt assumenda.
             </div>
-            <div className="product-main-about-item">
+            <div className="product-main-section-a-item">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit.
               Accusantium, eligendi quia, mollitia nobis eaque voluptates fugiat
               molestiae perspiciatis cupiditate expedita temporibus neque modi
               velit nesciunt assumenda.
             </div>
-            <div className="product-main-about-item">
+            <div className="product-main-section-a-item">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit.
               Accusantium, eligendi quia, mollitia nobis eaque voluptates fugiat
               molestiae perspiciatis cupiditate expedita temporibus neque modi
@@ -75,38 +92,130 @@ const Product = ({ data }) => {
             </div>
           </div>
         </div>
-        <div className="product-main-features">
-          <div className="product-main-features-title">Features</div>
+        <div className="product-main-section-b">
+          <div className="product-main-section-b-title">Features</div>
           <div className="cards">
             <div className="card">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-              Accusantium, eligendi quia, mollitia nobis eaque voluptates fugiat
-              molestiae perspiciatis cupiditate expedita temporibus neque modi
-              velit nesciunt assumenda.
+              <i className="icon fa fa-check-circle fa-2x"></i>
+              <div className="text-box">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Accusantium, eligendi quia, mollitia nobis eaque voluptates
+                fugiat molestiae perspiciatis cupiditate expedita temporibus
+                neque modi velit nesciunt assumenda.
+              </div>
             </div>
             <div className="card">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-              Accusantium, eligendi quia, mollitia nobis eaque voluptates fugiat
-              molestiae perspiciatis cupiditate expedita temporibus neque modi
-              velit nesciunt assumenda.
+              <i className="icon fa fa-check-circle fa-2x"></i>
+              <div className="text-box">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Accusantium, eligendi quia, mollitia nobis eaque voluptates
+                fugiat molestiae perspiciatis cupiditate expedita temporibus
+                neque modi velit nesciunt assumenda.
+              </div>
             </div>
             <div className="card">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-              Accusantium, eligendi quia, mollitia nobis eaque voluptates fugiat
-              molestiae perspiciatis cupiditate expedita temporibus neque modi
-              velit nesciunt assumenda.
+              <i className="icon fa fa-check-circle fa-2x"></i>
+              <div className="text-box">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Accusantium, eligendi quia, mollitia nobis eaque voluptates
+                fugiat molestiae perspiciatis cupiditate expedita temporibus
+                neque modi velit nesciunt assumenda.
+              </div>
             </div>
             <div className="card">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-              Accusantium, eligendi quia, mollitia nobis eaque voluptates fugiat
-              molestiae perspiciatis cupiditate expedita temporibus neque modi
-              velit nesciunt assumenda.
+              <i className="icon fa fa-check-circle fa-2x"></i>
+              <div className="text-box">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Accusantium, eligendi quia, mollitia nobis eaque voluptates
+                fugiat molestiae perspiciatis cupiditate expedita temporibus
+                neque modi velit nesciunt assumenda.
+              </div>
             </div>
           </div>
         </div>
-        <div className="product-main-video">
-          <div className="product-main-video-title">Video</div>
-          <div className="product-main-video-container"></div>
+        <div className="product-main-section-c">
+          <div className="product-main-section-c-title">
+            Switching To Complete Accounts Payable Solutions
+            <div className="product-main-section-c-title-sub">
+              Custermers reported
+            </div>
+          </div>
+
+          <div className="cards">
+            <div className="card">
+              <div className="c100 p70 center">
+                <span>70%</span>
+                <div className="slice">
+                  <div className="bar"></div>
+                  <div className="fill"></div>
+                </div>
+              </div>
+              <div className="text-box">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Accusantium, eligendi quia
+              </div>
+            </div>
+            <div className="card">
+              <div className="c100 p93 center">
+                <span>93%</span>
+
+                <div className="slice">
+                  <div className="bar"></div>
+
+                  <div className="fill"></div>
+                </div>
+              </div>
+              <div className="text-box">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Accusantium, eligendi quia
+              </div>
+            </div>
+            <div className="card">
+              <div className="c100 p89 center">
+                <span>89%</span>
+
+                <div className="slice">
+                  <div className="bar"></div>
+
+                  <div className="fill"></div>
+                </div>
+              </div>
+              <div className="text-box">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Accusantium, eligendi quia
+              </div>
+            </div>
+            <div className="card">
+              <div className="c100 p100 center">
+                <span>100%</span>
+
+                <div className="slice">
+                  <div className="bar"></div>
+
+                  <div className="fill"></div>
+                </div>
+              </div>
+              <div className="text-box">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Accusantium, eligendi quia
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="product-main-section-d">
+          <div className="product-main-section-d-title">Intergrations</div>
+          <div className="cards">
+            {data.images.edges.map(edge => {
+              return (
+                <div className="card">
+                  <Img
+                    fluid={edge.node.childImageSharp.fluid}
+                    className="img"
+                  />
+                </div>
+              )
+            })}
+          </div>
         </div>
       </div>
       <Footer></Footer>
@@ -115,24 +224,3 @@ const Product = ({ data }) => {
 }
 
 export default Product
-
-export const pageQuery = graphql`
-  query NewsArticleByID($id: String!) {
-    markdownRemark(id: { eq: $id }) {
-      id
-      html
-      frontmatter {
-        title
-        alias
-        description
-        featuredimage {
-          childImageSharp {
-            fluid(maxWidth: 1000, quality: 100) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-      }
-    }
-  }
-`
